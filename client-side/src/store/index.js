a1eac19c9d0809c68327c269c00131d5a5ce51fc
Muhammad Bagus Pramaskoro <pramaskoro@gmail.com>
2024-11-09 20:16:53 +0700

    Deleting Client Side

--- client-side/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import gameReducer from "./games";
-
-
-export const store = configureStore({
-    reducer: {
-        game: gameReducer,
-    },
-});
